06e6484e594beaed135ed18885ef9cbf55a24568
Fachrezy Duo <fachrezyofficial@gmail.com>
2026-01-23 11:11:04 +0700

    ganti title page

--- resources/views/pos/index.blade.php
+++ resources/views/pos/index.blade.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Revent - Centechno</title>
+    <title>REVENT - Centechno</title>
 
     <!-- Tailwind CSS CDN -->
     <script src="https://cdn.tailwindcss.com"></script>
